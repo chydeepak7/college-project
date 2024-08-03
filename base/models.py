426326a28f35607a370beb0d5387aa0b45ce4cce
chydeepak7 <deepakchy793@gmail.com>
2024-08-03 23:20:07 +0545

    changed database

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    numOfRoom = models.IntegerField()
+    address = models.CharField(max_length=100)
+    water = models.BooleanField()
+    electricity = models.BooleanField()
+    wifi = models.CharField(max_length=100)
+    bathroom = models.CharField(max_length=100)
+    otherDetails = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.address
+
+
+
+
+
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+'rest_framework',
     'base.apps.BaseConfig',
 ]
 
@@ -74,12 +75,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '',  # Set to empty string for localhost.
+        'PORT': '',       # Set to empty string for default.
     }
 }
+
 
 
 # Password validation
