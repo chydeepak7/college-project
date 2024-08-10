da36750a6933bdb5c0447ba37e9ad8bd2c8d449a
chydeepak7 <deepakchy793@gmail.com>
2024-08-10 10:03:39 +0545

    before registration

--- base/models.py
+++ base/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
 
 class Room(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     numOfRoom = models.IntegerField()
     address = models.CharField(max_length=100)
@@ -11,6 +13,7 @@ class Room(models.Model):
     wifi = models.CharField(max_length=100)
     bathroom = models.CharField(max_length=100)
     otherDetails = models.CharField(max_length=100)
+    createdAt = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.address
---
+++ base/urls.py
@@ -0,0 +1,14 @@
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+
+    path('api/users/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+]
+
